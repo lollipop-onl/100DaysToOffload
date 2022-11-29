c04adc839b06c637e7482ffc7ca0bef21b6f77dc
Ryoya Tamura <lollipop@simochee.net>
2022-11-29 23:47:05 +0900

    ✨ add tailwindcss

--- projects/2023/astro.config.ts
+++ projects/2023/astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   base: '/2023',
   trailingSlash: 'always',
+  integrations: [tailwind()]
 });
